ab6f8e5122697f64a3d53500d2684cbb63b7a5aa
LostwalletHq <SwiitWurld@gmail.com>
2026-07-12 17:17:15 +0100

    Add welcome message and button click event

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+// Lost Wallet Website
+console.log("Welcome to Lost Wallet!");
+
+const button = document.querySelector(".button");
+
+button.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    alert("🚀 Welcome to Lost Wallet!\n\nThe journey has just begun.");
+});
