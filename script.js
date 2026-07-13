f20ecea34b7929b01ccb25c85ae27d7259055280
LostwalletHq <SwiitWurld@gmail.com>
2026-07-13 17:13:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,26 @@ const walletStatus = document.getElementById("wallet-status");
 if (walletStatus) {
     walletStatus.textContent = "🟢 Still Missing";
 }
+const hunterCount = document.getElementById("hunter-count");
 
+if (hunterCount) {
+
+    let count = 0;
+    const target = 500;
+
+    const counter = setInterval(() => {
+
+        count++;
+
+        hunterCount.textContent = count;
+
+        if (count >= target) {
+            clearInterval(counter);
+        }
+
+    }, 5);
+
+}
 // Join Community Button
 const joinButton = document.querySelector(".btn");
 
